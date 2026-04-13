12a30d08db05c2928bd702c349d395ad85a80b75
icawaeo <veronica.waeo@gmail.com>
2026-04-13 12:06:31 +0800

    feat: redefine Building table structure and update operational_days column

--- prisma/migrations/20260404093000_add_building_model/migration.sql
+++ prisma/migrations/20260404093000_add_building_model/migration.sql
@@ -1,15 +1,2 @@
--- CreateTable
-CREATE TABLE "Building" (
-	"building_id" TEXT NOT NULL,
-	"building_name" TEXT NOT NULL,
-	"operational_days" TEXT[] DEFAULT ARRAY[]::TEXT[],
-	"open_time" TEXT NOT NULL,
-	"close_time" TEXT NOT NULL,
-	"createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	"updatedAt" TIMESTAMP(3) NOT NULL,
-
-	CONSTRAINT "Building_pkey" PRIMARY KEY ("building_id")
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Building_building_name_key" ON "Building"("building_name");
+-- AlterTable
+ALTER TABLE "Building" ALTER COLUMN "operational_days" DROP DEFAULT;
